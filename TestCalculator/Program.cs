2353f7cd058995e04c982e5f1413f0257307f0cd
Zealot0150 <per.claeson@gmail.com>
2021-12-20 15:19:04 +0100

    Add project files.

---
+++ TestCalculator/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using static System.Console;
+
+namespace TestCalculator
+{
+    public class Program
+    {
+        static double ReadUserDouble()
+        {
+            double userInput = 0;
+            bool hasUserMakedTheIput = false;
+            do
+            {
+                if (Double.TryParse(ReadLine(), out userInput))
+                    hasUserMakedTheIput = true;
+                else
+                    WriteLine("Felaktikigt format, ange ett tal");
+            }
+            while (!hasUserMakedTheIput);
+            return userInput;
+        }
+
+        static string ReadUserOperand()
+        {
+            string userInput = "";
+            string operatorList = "*/ +-";
+            bool hasUserMakedTheIput = false;
+            do
+            {
+                userInput = Console.ReadLine();
+
+
+                if (operatorList.Contains(userInput) && userInput.Length == 1)
+                    hasUserMakedTheIput = true;
+                else
+                    WriteLine("Felaktikigt format, ange någon av operanderna * / + -");
+
+            } while (!hasUserMakedTheIput);
+            return userInput;
+        }
+
+
+         static double Addition(double [] doubleArray)
+        {
+            double result = 0;
+            for (int i = 0; i < doubleArray.Length; i+=2)
+            {
+                result = Addition(i, i + 1);
+            }
+            return result;
+        }
+
+        public static double Subtraction(double[] doubleArray)
+        {
+            double result = 0;
+            for (int i = 0; i < doubleArray.Length; i += 2)
+            {
+                result = Subtraction(i, i + 1);
+            }
+            return result;
+        }
+
+        public static double Addition(double firstArgument, double secondArgument)
+        {
+            return firstArgument + secondArgument;
+        }
+
+        public static double Division(double firstArgument, double secondArgument)
+        {
+            if (secondArgument == 0)
+                return 0;
+            else
+                return firstArgument / secondArgument;
+        }
+
+        public static double Subtraction(double firstArgument, double secondArgument)
+        {
+            return firstArgument - secondArgument;
+        }
+
+
+        public static double Multiplication(double firstArgument, double secondArgument)
+        {
+            return firstArgument * secondArgument;
+        }
+
+        static void PrintResultToConsole(double firstArgument, double secondArgument, double result, string usedOperator)
+        {
+            Console.WriteLine($"{firstArgument} {usedOperator} {secondArgument} = {result}");
+        }
+
+        static void Main(string[] args)
+        {
+            bool running = true;
+            WriteLine("Välkommen till calculatorn version 1.0");
+            double firstArgument, secondArgument, result = 0;
+            string userOperator = "";
+            while (running)
+            {
+                WriteLine("Skriv in ditt första tal");
+                firstArgument = ReadUserDouble();
+                WriteLine("Skriv in operand, välj mellan * / + - ");
+                userOperator = ReadUserOperand();
+                WriteLine("Skriv in ditt andra tal");
+                secondArgument = ReadUserDouble();
+                switch (userOperator)
+                {
+                    case "*":
+                        result = Multiplication(firstArgument, secondArgument);
+                        break;
+                    case "/":
+                        if ((firstArgument > 0) && (secondArgument == 0))
+                            PrintDivsionError();
+                        else
+                            result = Division(firstArgument, secondArgument);
+                        break;
+                    case "+":
+                        result = Addition(firstArgument, secondArgument);
+                        break;
+                    case "-":
+                        result = Subtraction(firstArgument, secondArgument);
+                        break;
+
+                    default:
+                        WriteLine("Something strange has happened, call the suport");
+                        break;
+                }
+                PrintResultToConsole(firstArgument, secondArgument, result, userOperator);
+
+
+                WriteLine("Vill du avsluta, tryck y plus enter annars bara enter för ny beräkning");
+                if (ReadLine() == "y")
+                    Environment.Exit(0);
+            }
+
+        }
+
+        private static void PrintDivsionError()
+        {
+            WriteLine("Ogliltlig nämnare för division");
+        }
+    }
+}
---
+++ XUnitTestCalculator/XUnitTest.cs
@@ -0,0 +1,54 @@
+using System;
+using Xunit;
+using  TestCalculator;
+namespace XUnitTestCalculator
+{
+    public class XUnitTest
+    {
+        [Theory]
+        [InlineData(1,2,3)]
+        [InlineData(Double.NaN, Double.NaN, Double.NaN)]
+        [InlineData(1.2,2.3,3.5)]
+        public void TestAddition(double first, double second, double result)
+        {
+            double calcResult;
+            calcResult = Program.Addition(first, second);
+            Assert.Equal(result, calcResult);
+        }
+
+        [Theory]
+        [InlineData(1, 2, -1)]
+        [InlineData(Double.NaN, Double.NaN, Double.NaN)]
+        [InlineData(10.5, 2.3, 8.2)]
+        public void TestSubtraction(double first, double second, double result)
+        {
+            double calcResult;
+            calcResult = Program.Subtraction(first, second);
+            Assert.Equal(result, calcResult);
+        }
+
+        [Theory]
+        [InlineData(1, 0, 0)]
+        [InlineData(1, 2, 2)]
+        [InlineData(Double.NaN, Double.NaN, Double.NaN)]
+        [InlineData(10.5, 10, 105)]
+        public void TestMulti(double first, double second, double result)
+        {
+            double calcResult;
+            calcResult = Program.Multiplication(first, second);
+            Assert.Equal(result, calcResult);
+        }
+
+        [Theory]
+        [InlineData(4, 4, 1)]
+        [InlineData(Double.NaN, Double.NaN, Double.NaN)]
+        [InlineData(100.1, 10 , 10.01)]
+        [InlineData(0, 10, 0)]
+        public void TestDiv(double first, double second, double result)
+        {
+            double calcResult;
+            calcResult = Program.Division(first, second);
+            Assert.Equal(result, calcResult);
+        }
+    }
+}
